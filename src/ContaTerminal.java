d738496e76ebae561f3eb8ee1403550de8c986d3
IvambergSilva <ivambergisilva@gmail.com>
2024-03-13 17:51:36 -0300

    :sparkles: feat: Adição da ContaTerminal

--- src/ContaTerminal.java
+++ src/ContaTerminal.java
@@ -1,5 +1,22 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+
+        System.out.print("Digite o seu NOME: ");
+        String name = scanner.next();
+
+        System.out.print("Digite o número da sua AGÊNCIA: ");
+        String agency = scanner.next();
+
+        System.out.print("Digite o número da sua CONTA: ");
+        int account = scanner.nextInt();
+
+        System.out.print("Digite o valor do seu SALDO: ");
+        Double balance = scanner.nextDouble();
+    
+        System.out.println("Olá " + name + ", obrigado por criar uma conta em nosso banco, sua agência é " + agency + ", conta " + account + " e seu saldo de R$ " + balance + " já está disponível para saque.");
     }
 }
